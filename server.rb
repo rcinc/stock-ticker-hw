83dee0ed1789ab69b883f768f810687f76d0654a
Robert Cardona <rfcardona@gmail.com>
2015-07-16 17:31:29 -0400

    deploying web app

---
+++ server.rb
@@ -0,0 +1,20 @@
+require "HTTParty"
+require "nokogiri"
+require "sinatra"
+
+
+response = HTTParty.get("http://finance.yahoo.com/q?s=AAPL")
+
+dom = Nokogiri::HTML(response.body)
+puts dom.class
+
+stock_price = dom.xpath('//*[@id="yfs_l84_aapl"]').first.content
+
+prev_price = dom.xpath('//*[@class="yfnc_tabledata1"]').first.content
+
+# puts stock_price
+# puts prev_price
+
+get '/' do
+"Apple's stock is at #{stock_price} but it was #{prev_price} yesterday."
+end
